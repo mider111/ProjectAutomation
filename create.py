4d73eaf658fb100971a89790d1e14e6a6a2f3ea3
Viktor Ivanov <vik.ivanov98@gmail.com>
2020-07-12 17:30:18 -0400

    Added more commands

--- create.py
+++ create.py
@@ -5,14 +5,25 @@ project_dir = os.environ.get('PD')
 github_token = os.environ.get('GH')
 
 folder = sys.argv[1]
-dir = project_dir + '/' + folder
+_dir = project_dir + '/' + folder
+os.chdir(project_dir)
+check = not os.path.exists(folder)
 
-os.mkdir(dir)
-os.chdir(dir)
+if check:
+    try:
+        os.mkdir(_dir)
+        os.chdir(_dir)
+    except FileExistsError as error:
+        print(error)
+else:
+    print("A folder with that name already exists!")
 
 commands = [
-   # f'echo #{}'
+    f'echo #{folder} > README.md'
 ]
+
+for c in commands:
+    os.system(c)
 
 
 
